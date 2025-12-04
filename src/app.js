49e2552220b50cebaa78172d3af686e3ceed6b8f
Cristian Cortes <cristianandrescguisao@gmail.com>
2025-12-04 07:17:30 -0500

    feat: añadir estructura ul y boton e implementar funcion agregarItem

--- src/app.js
+++ src/app.js
@@ -8,4 +8,18 @@ export function suma(a, b) {
     return a + b;
 }
 
+/**
+ * Agrega un nuevo <li> a la lista con id="lista"
+ * @param {string} texto - El texto del nuevo ítem de la lista
+ */
+export function agregarItem(texto) {
+    const lista = document.getElementById('lista');
+    if (lista) {
+        const nuevoItem = document.createElement('li');
+        nuevoItem.textContent = texto;
+        lista.appendChild(nuevoItem);
+        console.log(`Se agregó: ${texto}`);
+    }
+}
+
 console.log("App lista");
